5e666a30c0bf951ca9bf908d2b0e759c02ec3971
antroma <antonio.roman@alumnos.uva.es>
2017-12-13 10:42:51 +0100

    Fixed problem with interpreter order at examples

--- assets/search_methods.py
+++ assets/search_methods.py
@@ -1,24 +1,24 @@
-miString = "Soy una cadena una cadena String 100"
-miString.count("una cadena")
->>> 2
-miString.find("una")
->>> 16
-miString.startswith("Soy")
->>> True
-miString.endswith("Soy")
->>> False
-miString = "micadenaalfanumerica288"
-miString.isalnum()
->>> False
-miString.isalpha()
->>> False
-miString.isdigit()
->>> False
-miString = "soytodominusculas"
-miString.islower()
->>> False
-miString.isupper()
->>> False
-miString = "    "
-miString.isspace()
->>> True
+>>> miString = "Soy una cadena una cadena String 100"
+>>> miString.count("una cadena")
+2
+>>> miString.find("una")
+16
+>>> miString.startswith("Soy")
+True
+>>> miString.endswith("Soy")
+False
+>>> miString = "micadenaalfanumerica288"
+>>> miString.isalnum()
+False
+>>> miString.isalpha()
+False
+>>> miString.isdigit()
+False
+>>> miString = "soytodominusculas"
+>>> miString.islower()
+False
+>>> miString.isupper()
+False
+>>> miString = "    "
+>>> miString.isspace()
+True
--- assets/substitution_methods.py
+++ assets/substitution_methods.py
@@ -1,11 +1,11 @@
-miString = "Soy una cadena"
-miString.format()
->>> 'Soy una cadena'
-miString.replace("una", "la")
->>> 'Soy la cadena'
-miString.strip("a")
->>> 'Soy una caden'
-miString.lstrip("S")
->>> 'oy una cadena'
-miString.rstrip("S")
->>> 'Soy una cadena'
+>>> miString = "Soy una cadena"
+>>> miString.format()
+'Soy una cadena'
+>>> miString.replace("una", "la")
+'Soy la cadena'
+>>> miString.strip("a")
+'Soy una caden'
+>>> miString.lstrip("S")
+'oy una cadena'
+>>> miString.rstrip("S")
+'Soy una cadena'
